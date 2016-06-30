2e7b80a1c6dc896e306d8ecb287bd5e6c77579ec
Tanner Hobson <thobson125@gmail.com>
2016-06-30 16:40:14 -0400

    Start implementing histogram slices in time

--- reduce_EQSANS.py
+++ reduce_EQSANS.py
@@ -16,6 +16,30 @@ import plotly.graph_objs
 
 import numpy as np
 
+def restructure_histogram_data(data, det_width, det_height, subdivs=8):
+    # Not entirely sure why this needs to be here. As it is now, it rearranges
+    # some of the columns so that they match the 2D space of the detector
+    # correctly.
+    #
+    # For example, when subdivs = 8 and det_width = 192, then we take the
+    # detector and split it into 8 groups along the x axis, meaning that we end
+    # up with 192/8=24 columns in every group. Then we want to take the first
+    # group, and the 8//2+1=5th group and arrange them next to each other, and
+    # so on for the rest of the columns (2 and 6, 3 and 7, 4 and 8).
+    #
+    # You can experiment with different subdivs, but this seems to be working
+    # correctly, although it's definitely a hack.
+    indices = [x+y*subdivs//2 for x in range(subdivs // 2) for y in range(2)]
+
+    # Apply the subdivisions as well as reshape the 1 dimensions Y values that
+    # we get from Mantid into a 2D matrix of shape (height, width)
+    data = data.reshape(det_width // subdivs, subdivs, det_height)
+    data = data.T[:, indices, :].T
+    data = data.reshape(det_width, det_height)
+    data = data.T
+
+    return data
+
 def main(filename, outdir, output_type, include_plotly_js, plot_type, bin_width,
          num_bins):
     # Load the workspace
@@ -35,58 +59,80 @@ def main(filename, outdir, output_type, include_plotly_js, plot_type, bin_width,
     else:
         num_bins = blocksize // bin_width
 
-    print('bin_width', bin_width)
-    print('num_bins', num_bins)
+    maximum = 0
 
-    # Rebin data
-    rebinned_workspace = mantid.simpleapi.Rebin(
-        workspace,
-        Params='{},{},{}'.format(0, bin_width, bin_width * num_bins),
-    )
+    if plot_type == 'tof' or plot_type == 'both':
+        # Rebin data
+        rebinned_workspace = mantid.simpleapi.Rebin(
+            workspace,
+            Params='{},{},{}'.format(0, bin_width, bin_width * num_bins),
+            FullBinsOnly=True,
+        )
 
-    print('blocksize2', rebinned_workspace.blocksize())
+        rebinned_y = rebinned_workspace.extractY()
+        maximum = max(maximum, np.log(np.max(rebinned_y)))
 
-    # Integrate along the time dimension to consolidate the data
-    integration_workspace = mantid.simpleapi.Integration(rebinned_workspace)
+        all_rebinned = [
+            restructure_histogram_data(rebinned_y[:, i], det_width, det_height)
+            for i in range(num_bins)
+        ]
 
-    # Not entirely sure why this needs to be here. As it is now, it rearranges
-    # some of the columns so that they match the 2D space of the detector
-    # correctly.
-    #
-    # For example, when subdivs = 8 and det_width = 192, then we take the
-    # detector and split it into 8 groups along the x axis, meaning that we end
-    # up with 192/8=24 columns in every group. Then we want to take the first
-    # group, and the 8//2+1=5th group and arrange them next to each other, and
-    # so on for the rest of the columns (2 and 6, 3 and 7, 4 and 8).
-    #
-    # You can experiment with different subdivs, but this seems to be working
-    # correctly, although it's definitely a hack.
-    subdivs = 8
-    indices = [x+y*subdivs//2 for x in range(subdivs // 2) for y in range(2)]
+    if plot_type == 'integrated' or plot_type == 'both':
+        # Integrate along the time dimension to consolidate the data
+        integration_workspace = mantid.simpleapi.Integration(workspace)
 
-    # Apply the subdivisions as well as reshape the 1 dimensions Y values that
-    # we get from Mantid into a 2D matrix of shape (height, width)
-    integrated = integration_workspace.extractY()
-    integrated = integrated.reshape(det_width // subdivs, subdivs, det_height)
-    integrated = integrated.T[:, indices, :].T
-    integrated = integrated.reshape(det_width, det_height)
-    integrated = integrated.T
+        # Get Y data
+        integrated_y = integration_workspace.extractY()
+        maximum = max(maximum, np.log(np.max(integrated_y)))
 
-    # Mask out any values that will make taking the log difficult
-    Z = np.ma.masked_where((0 < integrated) & (integrated < np.e), integrated)
-    Z = np.ma.log(Z)
+        # Restructure Y data
+        integrated = restructure_histogram_data(integrated_y, det_width, det_height)
 
-    # Actually plot the data
-    trace = plotly.graph_objs.Heatmap(
-        z=Z,
-    )
+    rebinned_traces = []
+    if plot_type == 'tof' or plot_type == 'both':
+        for i, rebinned in enumerate(all_rebinned):
+            # Mask out any values that will make taking the log difficult
+            mask = (0 < rebinned) & (rebinned < np.e)
+            rebinned_z = np.ma.masked_where(mask, rebinned)
+            rebinned_z = np.ma.log(rebinned_z)
+
+            # Actually plot the data
+            rebinned_trace = plotly.graph_objs.Heatmap(
+                z=rebinned_z,
+                zmax=maximum,
+                visible=True if (i == 0) else 'legendonly',
+                showlegend=True,
+                name='Rebinned {}'.format(i),
+            )
+
+            rebinned_traces.append(rebinned_trace)
+
+    integrated_traces = []
+    if plot_type == 'integrated' or plot_type == 'both':
+        # Mask out any values that will make taking the log difficult
+        mask = (0 < integrated) & (integrated < np.e)
+        integrated_z = np.ma.masked_where(mask, integrated)
+        integrated_z = np.ma.log(integrated_z)
+
+        integrated_trace = plotly.graph_objs.Heatmap(
+            z=Z,
+            zmax=maximum,
+        )
+
+        integrated_traces.append(integrated_trace)
 
     layout = plotly.graph_objs.Layout(
         xaxis=dict(title='Tube'),
         yaxis=dict(title='Pixel'),
+        showlegend=True,
     )
 
-    fig = plotly.graph_objs.Figure(data=[trace], layout=layout)
+    print(len(integrated_traces + rebinned_traces))
+
+    fig = plotly.graph_objs.Figure(
+        data=integrated_traces + rebinned_traces,
+        layout=layout,
+    )
     div = plotly.offline.plot(fig, output_type='div', include_plotlyjs=True)
 
     # Save image to the disk
